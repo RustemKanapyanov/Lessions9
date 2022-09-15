fef70fb2d4bc06a3137614d77a829a8fdf18fda6
rustem <tkanapyanov@gmail.com>
2022-09-15 22:05:40 +0600

    task 66, 68 done

---
+++ TAKS66/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 66: Задайте значения M и N. 
+ Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+ M = 1; N = 15 -> 120
+ M = 4; N = 8. -> 30
+ */
+
+int M = InputNumbers("Введите M: ");
+int N = InputNumbers("Введите N: ");
+int temp = M;
+PrintSumm(M, N, temp = 0);
+
+if (M > N)
+{
+    M = N;
+    N = temp;
+}
+
+void PrintSumm(int M, int N, int sum)
+{
+    sum = sum + N;
+    if (N <= M)
+    {
+        Console.Write($"Сумма элементов = {sum} ");
+        return;
+    }
+    PrintSumm(M, N - 1, sum);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
---
+++ TASK68/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29
+ */
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return A(m - 1, 1);
+    else return A(m - 1, A(m, n - 1));
+}
+Console.WriteLine("Введите m для функции Аккермана:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n для функции Аккермана:");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n < 0) ^ (m < 0))
+{
+    Console.WriteLine("Числа не могут быть отрицательными");
+}
+else
+{
+    Console.WriteLine($"Функция аккремана при m={m} n={n} равняется {A(m, n)}");
+}
